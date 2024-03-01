da78331053d6e967977ec66133963e220832a872
Raf Sz <raf.sz@icloud.com>
2024-03-01 14:51:36 +0000

    Improve postBuild.mjs example

--- examples/using-script/scripts/postBuild.mjs
+++ examples/using-script/scripts/postBuild.mjs
@@ -1,9 +1,21 @@
 import path from 'path';
 import * as fs from 'fs';
 import { generateRedirects } from 'next-redirect-generator';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'path';
 
-const outDir = path.join(process.cwd(), 'out');
+// Redirects
+const pagesFolder = join(process.cwd(), 'pages');
 
-const pagesFolder = path.join(process.cwd(), 'pages');
+// Generated redirects in chosen format.
 const redirects = generateRedirects(pagesFolder, '_redirects');
-fs.writeFileSync(path.join(outDir, '_redirects'), redirects);
+
+// Only add file if something has been generated.
+if(redirects.trim()) {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const dest = join(__dirname, '../out/_redirects');
+
+  fs.writeFileSync(dest, redirects)
+  console.log('Wrote', dest);
+}
+
